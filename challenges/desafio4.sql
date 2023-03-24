c7abe43e4681f138e5249dde67c24607d93effd7
Ana Paula Aguiar <paulaaguiars16@gmail.com>
2023-03-24 10:24:36 -0300

    Fix: Desafio 4

--- challenges/desafio4.sql
+++ challenges/desafio4.sql
@@ -1,8 +1,7 @@
 SELECT DISTINCT(u.nome) AS pessoa_usuaria,
-	CASE
-		WHEN h.data_visualizacao BETWEEN YEAR(2021) AND (NOW()) THEN 'Ativa'
-		ELSE 'Inativa'
-END AS 'status_pessoa_usuaria'
+IF(MAX(YEAR(h.data_visualizacao)) > 2020, 'Ativa', 'Inativa') AS status_pessoa_usuaria
 FROM SpotifyClone.usuario AS u
 INNER JOIN SpotifyClone.historico AS h
+ON u.usuario_id = h.usuario_id
+GROUP BY pessoa_usuaria
 ORDER BY pessoa_usuaria;
